fa627a14fdc3e500f7fb2f0aca24a7fefdc32e74
Mathias Sejje <mathiassejje@gmail.com>
2025-11-21 21:14:43 -0500

    actualizacion bootstrap

--- JavaScript/main.mjs
+++ JavaScript/main.mjs
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './router.mjs';
 import './updateHeader.mjs'
 import { router } from './router.mjs';  
